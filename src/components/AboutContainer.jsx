e5925d439c9f8099584e7eb7b8fd7d3d83c5ef11
André Leal <57924518+AndreLeal950@users.noreply.github.com>
2022-07-19 18:34:07 -0300

    Correção de grafia no paragrafo Sobre

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -4,7 +4,7 @@ const AboutContainer = () => {
   return (
     <section className="about-container">
       <h2>Sobre</h2>
-      <p>Me chamo André Leal, tenho 48 anos, casado a vinte e oito anos,
+      <p>Me chamo André Leal, tenho 48 anos, casado ha vinte e oito anos,
         temos duas filhas.Confessso, que a programação foi uma das  tarefas
         mais difíceis  que  me  propus aprender. Me desafiei, persisti e consegui
         virar a chave. Hoje estou em busca de uma recolocação no mercado de trabalho,
